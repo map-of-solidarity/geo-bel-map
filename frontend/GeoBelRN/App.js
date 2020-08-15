d7aed268160fa0fce80bc744de1fe866bac85146
paalex <salexp@gmail.com>
2020-08-15 10:27:22 -0400

    working ios and android - webview

--- frontend/GeoBelRN/App.js
+++ frontend/GeoBelRN/App.js
@@ -8,10 +8,20 @@
 import React, {Component} from 'react';
 import {StyleSheet, View, SafeAreaView} from 'react-native';
 import {WebView} from 'react-native-webview';
+import ArcGISMapView from 'react-native-arcgis-mapview';
+
 const arcgisURL = 'https://geobel.maps.arcgis.com/apps/webappviewer/index.html?id=1a81ea9392954fbfa3eb6cec81f4da55';
 type Props = {};
 export default class App extends Component<Props> {
   render() {
+    // return (
+    //   <ArcGISMapView
+    //     ref={mapView => this.mapView = mapView}
+    //     initialMapCenter={[{latitude: 53.900566, longitude: 27.558931}]}
+    //     basemapUrl={''}
+    //     // your props here
+    //   />
+    // );
     return (
       <SafeAreaView style={styles.container}>
         <WebView
