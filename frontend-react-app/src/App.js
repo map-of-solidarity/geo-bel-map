5332bfb824635472342973d70b3fe20117c6aedf
iamklim <drkleem@gmail.com>
2020-08-15 14:40:18 +0300

    create-react-app initial setup

---
+++ frontend-react-app/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.scss';
+import WebMapView from "./components/WebMapView/WebMapView";
+
+function App() {
+  return (
+    <div className="App">
+      <p>rafasd</p>
+      <WebMapView />
+    </div>
+  );
+}
+
+export default App;
