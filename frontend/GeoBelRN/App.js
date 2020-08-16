ed5865eb1235bf1791ff8582d578f367c3403913
paalex <salexp@gmail.com>
2020-08-16 18:01:55 -0400

    web app V2

--- frontend/GeoBelRN/App.js
+++ frontend/GeoBelRN/App.js
@@ -7,10 +7,11 @@
  */
 import React, {Component} from 'react';
 import {StyleSheet, View, SafeAreaView} from 'react-native';
-import {WebView} from 'react-native-webview';
-import ArcGISMapView from 'react-native-arcgis-mapview';
+import {WebView, Linking} from 'react-native-webview';
+// import ArcGISMapView from 'react-native-arcgis-mapview';
 
-const arcgisURL = 'https://geobel.maps.arcgis.com/apps/webappviewer/index.html?id=1a81ea9392954fbfa3eb6cec81f4da55';
+// const arcgisURL = 'https://geobel.maps.arcgis.com/apps/webappviewer/index.html?id=1a81ea9392954fbfa3eb6cec81f4da55'; // dev
+const arcgisURL = 'https://geobel.maps.arcgis.com/apps/webappviewer/index.html?id=d20dff912ddd4af6a265dad0d0d782fb'; // prod
 type Props = {};
 export default class App extends Component<Props> {
   render() {
@@ -45,3 +46,10 @@ const styles = StyleSheet.create({
     // paddingVertical: 30
   }
 });
+
+// onNavigationStateChange={(event) => {
+//   if (event.url !== arcgisURL && event.navigationType === 'click') {
+//     this.webview.stopLoading();
+//     Linking.openURL(event.url);
+//   }
+// }}
