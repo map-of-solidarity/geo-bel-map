a2373e5af4dec2c0a825f2a0addc59de868f6352
paalex <salexp@gmail.com>
2020-08-16 01:13:07 -0400

    fix push to arcgis

--- telegram-client/layerPush/index.ts
+++ telegram-client/layerPush/index.ts
@@ -1,5 +1,5 @@
 import { MessageLocation, MessageType, Photo } from '../prepareMessage/types';
-
+import FormData from 'form-data';
 const axios = require('axios').default;
 const API_URL =
   'https://services5.arcgis.com/XV4SHAr4F9IApZBE/arcgis/rest/services/Crowdsourced2/FeatureServer/0/addFeatures';
@@ -23,15 +23,20 @@ export const layerPush = async (readyMessage: IReadyMessage) => {
     if (location) {
       const { long, lat } = location;
 
-      const feature = {
+      const features = {
         geometry: { x: long, y: lat },
         attributes: { Title: type, Description: text },
       };
-
-      const response = await axios.post(API_URL, {
-        features: feature,
-        f: 'json',
-      });
+      const formData = new FormData();
+      formData.append('features', JSON.stringify(features));
+      formData.append('f', 'json');
+      formData.append('key', '');
+      const response = await axios.post(API_URL, formData,
+        {
+          // You need to use `getHeaders()` in Node.js because Axios doesn't
+          // automatically set the multipart form boundary in Node.
+          headers: formData.getHeaders()
+        });
 
       console.log(`[layerPush] response:`);
       console.log(response);
