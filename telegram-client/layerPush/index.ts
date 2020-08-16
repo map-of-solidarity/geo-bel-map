8af951fd8de26e16b53fdeba5d6576480055f695
iamklim <drkleem@gmail.com>
2020-08-16 04:55:52 +0300

    sending one feature object

--- telegram-client/layerPush/index.ts
+++ telegram-client/layerPush/index.ts
@@ -3,17 +3,6 @@ import { MessageLocation, MessageType, Photo } from '../prepareMessage/types';
 const axios = require('axios').default;
 const API_URL =
   'https://services5.arcgis.com/XV4SHAr4F9IApZBE/arcgis/rest/services/Crowdsourced2/FeatureServer/0/addFeatures';
-const mockedFeature = [
-  {
-    geometry: { x: -118.15, y: 33.8 },
-    attributes: {
-      OWNER: 'Joe Smith',
-      VALUE: 94820.37,
-      APPROVED: true,
-      LASTUPDATE: 1227663551096,
-    },
-  },
-];
 
 interface IReadyMessage {
   chatId: number;
@@ -29,18 +18,19 @@ export const layerPush = async (readyMessage: IReadyMessage) => {
   console.log(`[layerPush] prepared message received for webMap layer push`);
 
   try {
-    const { date, text, location, photo } = readyMessage;
+    const { date, text, location, photo, type } = readyMessage;
 
     if (location) {
       const { long, lat } = location;
 
       const feature = {
         geometry: { x: long, y: lat },
-        attributes: { status: 'alive' },
+        attributes: { Title: type, Description: text },
       };
 
       const response = await axios.post(API_URL, {
-        features: [feature],
+        features: feature,
+        f: 'json',
       });
 
       console.log(`[layerPush] response:`);
