e20bc70a0fbadf66b166f52e5895eeabdf762880
Vania Kucher <dev.kucher@gmail.com>
2020-08-18 04:28:23 +0300

    chore

--- telegram-client/middleware/events/newMessage.ts
+++ telegram-client/middleware/events/newMessage.ts
@@ -1,12 +1,12 @@
-import { Message } from "airgram"
 import PreparedPhotoMessage from "../../prepareMessage/photoMessage";
 import PreparedTextMessage from "../../prepareMessage/textMessage";
+import PreparedVideoMessage from "../../prepareMessage/videoMessage";
 import { airgram } from '../../index'
 import { setupMessage } from '../../helpers/setupMessage';
 import { validateMessage } from '../../helpers/validateMessage'
 
 interface IOnNewMessage {
-    (handler: (message: PreparedPhotoMessage | PreparedTextMessage) => void): void
+    (handler: (message: PreparedPhotoMessage | PreparedTextMessage | PreparedVideoMessage) => void): void
 }
 
 export const onNewMessage: IOnNewMessage = (handler) => {
