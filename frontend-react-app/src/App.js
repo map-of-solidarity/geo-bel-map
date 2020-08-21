e875d2b37701970f903d038301ceff04f4d010bf
paalex <paalex@users.noreply.github.com>
2020-08-21 12:08:36 -0400

    Update App.js

--- frontend-react-app/src/App.js
+++ frontend-react-app/src/App.js
@@ -9,7 +9,7 @@ export default class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      telegramFolded: true,s
+      telegramFolded: true,
       telegramClicked: false
     }
   }
