cf25d4c7055cdb1259f7e0bf0f07ae2dc62aa043
paalex <salexp@gmail.com>
2020-08-24 21:55:45 -0400

    firebase ios

--- frontend/GeoBelRN/ios/GeoBelRN/AppDelegate.m
+++ frontend/GeoBelRN/ios/GeoBelRN/AppDelegate.m
@@ -3,7 +3,7 @@
 #import <React/RCTBridge.h>
 #import <React/RCTBundleURLProvider.h>
 #import <React/RCTRootView.h>
-
+//#import <Firebase.h>
 #ifdef FB_SONARKIT_ENABLED
 #import <FlipperKit/FlipperClient.h>
 #import <FlipperKitLayoutPlugin/FlipperKitLayoutPlugin.h>
